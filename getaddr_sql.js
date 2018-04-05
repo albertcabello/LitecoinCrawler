6ca8f5d03f17f34c872a2568ce68a108e756b28f
Alberto Cabello <albert.a.cabello@gmail.com>
2018-04-05 11:08:05 -0400

    Finished the SQL server code and queries.  Outgoing connections are now logged to a table

--- getaddr_sql.js
+++ getaddr_sql.js
@@ -10,15 +10,45 @@ var arrayPush = function(arr, callback) {
 	};
 };
 
+var connection = mysql.createConnection({
+	host	 : config.db.host,
+	user	 : config.db.user,
+	password : config.db.password,
+	database : config.db.database,
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.log("Error connecting to mysql server", err);
+		return;
+	}
+	console.log("Connected to mysql!");
+	
+	//Begin pushing to the queue
+});
+
 process.on('uncaughtException', function(exception) { //Update mysql table success column with 0 for the current ip since this will only get called on bad connection
-	process.exit();
+	if (exception.address != undefined) {
+		var query = `insert into ${config.db.table} (ip, success, error) values('${exception.address}', 0, '${exception.errno}')`;
+		connection.query(query, function (err, result, field) {
+			if (err) {
+				console.log("Error:", err);
+			}
+		});
+	}
 });
 
 var peers = {}; //Map of peers
 var queue = []; //Pending peers
 
+
 arrayPush(queue, function(updatedArr) {
-	var peer = new Peer({host: queue[0]}); //Get front of the queue
+	var peer = new Peer({host: queue[0]});
+	var versionMessage;
+
+	peer.on('version', function(message) {
+		versionMessage = JSON.stringify(message);
+	});
 
 	peer.on('ready', function() { //Update the mysql table
 		console.log("Connected to", peer.host);
@@ -26,15 +56,22 @@ arrayPush(queue, function(updatedArr) {
 		var message = messages.GetAddr();
 		peer.sendMessage(message);
 	});
-
+	
 	peer.on('addr', function(message) {
-		console.log(peer.host, "has received a getaddr message");
+		var addresses = "";
 		message.addresses.forEach(function(address) {
+			addresses += address.ip.v4 + ",";
 			queue.push(address.ip.v4);
+		});
+		var query = `insert into ${config.db.table} (ip, success, getaddr, version) values('${peer.host}', 1, '${addresses}', '${versionMessage}');`
+		connection.query(query, function(err, results, fields) {
+			if (err) {
+				console.log("Error, can't guarantee addition to database", err);
+			}
 		});
 	});
 
-	peer.connect(); //Connect to the peer if possible
+	peer.connect();
 
 	peers[queue.shift()] = peer; //Pop front of queue and add it to the map
 });

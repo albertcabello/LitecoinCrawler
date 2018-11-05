3477621c06c1b01cff33a82e1e4ead2333679984
Alberto Cabello <albert.a.cabello@gmail.com>
2018-11-05 16:19:08 -0500

    Listening server now uses shared peer events for consistent logging everywhere

--- listener/listener.js
+++ listener/listener.js
@@ -1,12 +1,14 @@
-var Peer = require('litecore-p2p').Peer;
-var Pool = require('litecore-p2p').Pool;
-var Networks = require('litecore-lib').Networks;
-var Messages = require('litecore-p2p').Messages;
 var config = require('./config.js');
 var net = require('net');
 
+var sharedPeerLibrary = require('../shared/peer.js');
 var modBN = require('../shared/modBN.js');
 var db = require('../shared/sql.js');
+
+var Peer = sharedPeerLibrary.litecore_p2p.Peer;
+var Pool = sharedPeerLibrary.litecore_p2p.Pool;
+var Networks = sharedPeerLibrary.litecore_lib.Networks;
+var Messages = sharedPeerLibrary.litecore_p2p.Messages;
 
 var BN = modBN.BN;
 
@@ -28,30 +30,6 @@ Messages.prototype._buildFromBuffer = function(command, payload) {
  ***********************************************/
 var peers = {};
 
-function addPeerEvents(peer) {
-	var versionMessage;
-	peer.connectTries = 0;
-	peer.on('error', function(error) { //Peer is unreachable;
-		console.log("Crawler: Error reaching", peer.host, error.errno);
-	});
-
-	peer.on('disconnect', function() {
-		delete peers[peer.host];
-		setTimeout(function() {
-			if (peer.connectTries < 2) { //Connect three times, if we got here, it's been 1 already
-				console.log("Crawler: Connecting to", peer.host, "for the", peer.connectTries+2, "time");
-				peer.connectTries++;
-				peer.connect();
-			}
-		}, 5000);
-	});
-
-	peer.on('ready', function() { //Update the mysql table
-		console.log("Crawler: Connected to", peer.host);
-		peers[peer.host] = peer;
-	});
-}
-
 var serverPeers = [];
 var server = net.createServer(function(socket) {
 	console.log("Server: Incoming connection from", socket.remoteAddress);
@@ -63,6 +41,15 @@ var server = net.createServer(function(socket) {
 		console.log("Could not turn connection to peer, maybe it's not a peer?", e);
 		socket.destroy();
 	}
+	sharedPeerLibrary.addPeerEvents(peer);
+	//Custom peer handling
+	peer.on('disconnect', function() {
+		delete peers[peer.host];
+	});
+	peer.on('ready', function() {
+		peers[peer.host] = peer;
+	});
+
 	addPeerEvents(peer);
 	if (peer.host.startsWith("::ffff")) {
 		peer.host = peer.host.substring(7);
